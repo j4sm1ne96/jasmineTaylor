de52afc2a6aecdfae9f382aa6f567e237a865b73
Jasmine Taylor <83115753+j4sm1ne96@users.noreply.github.com>
2022-01-29 13:04:24 +0000

    Update script.js

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -1,79 +1,78 @@
-$('#capitalBtn').on("click", function() {
+$(document).ready(function() {
+    $('#capitalBtn').on("click", function() {
+        $.ajax({
+            url: "libs/php/capitalInfo.php",
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                country: $('#capCountry').val(),
+            },
+            success: function(result) {
 
-    $.ajax({
-        url: "libs/php/getCountryInfo.php",
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            country: $('#capCountry').val(),
-        },
-        success: function(result) {
+                console.log(JSON.stringify(result));
 
-            console.log(JSON.stringify(result));
+                if (result.status.name == "ok") {
 
-            if (result.status.name == "ok") {
-
-                $('#txtCapital').html(result['data'][0]['capital']);  
+                    $('#txtCapital').html(result['data'][0]['capital']);  
+                }
+            
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                // your error code
             }
-        
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // your error code
-        }
-    }); 
+        }); 
+    });
 
 });
 
-$('#populationBtn').on("click", function() {
+$(document).ready(function() {
+    $('#populationBtn').on("click", function() {
+        $.ajax({
+            url: "libs/php/populationInfo.php",
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                country: $('#popCountry').val(),
+            },
+            success: function(result) {
 
-    $.ajax({
-        url: "libs/php/getCountryInfo.php",
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            country: $('#popCountry').val(),
-        },
-        success: function(result) {
+                console.log(JSON.stringify(result));
 
-            console.log(JSON.stringify(result));
+                if (result.status.name == "ok") {
 
-            if (result.status.name == "ok") {
-
-                $('#txtCapital').html(result['data'][0]['population']);  
+                    $('#txtCapital').html(result['data'][0]['population']);  
+                }
+            
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                // your error code
             }
-        
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // your error code
-        }
-    }); 
-
+        }); 
+    });
 });
 
-$('#continentBtn').on("click", function() {
+$(document).ready(function() {
+    $('#continentBtn').on("click", function() {
+        $.ajax({
+            url: "libs/php/continentInfo.php",
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                country: $('#capCountry').val(),
+            },
+            success: function(result) {
 
-    $.ajax({
-        url: "libs/php/getCountryInfo.php",
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            country: $('#capCountry').val(),
-        },
-        success: function(result) {
+                console.log(JSON.stringify(result));
 
-            console.log(JSON.stringify(result));
+                if (result.status.name == "ok") {
 
-            if (result.status.name == "ok") {
-
-                $('#txtContinent').html(result['data'][0]['continent']);  
+                    $('#txtContinent').html(result['data'][0]['continent']);  
+                }
+            
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                // your error code
             }
-        
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // your error code
-        }
-    }); 
-
+        }); 
+    });
 });
-
-
