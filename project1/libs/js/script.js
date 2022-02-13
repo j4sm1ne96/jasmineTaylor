85ec2a2b856764a8838d88123ccd85aceb9b6fa2
Jasmine Taylor <83115753+j4sm1ne96@users.noreply.github.com>
2022-02-13 22:22:24 +0000

    Create script.js

---
+++ project1/libs/js/script.js
@@ -0,0 +1,55 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer.provider('Stamen.Terrain').addTo(map);
+
+var Stamen_Terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 0,
+	ext: 'png'
+});
+
+
+//Country Modal
+//Getting Country Modal
+var modalCountry = document.getElementById("modalCountry");
+//Getting Country Modal Btn
+var countryBtn = document.getElementById("countryBtn");
+//Get Close Country Modal
+var closeBtn = document.getElementById("closeBtnCountry");
+//Open Modal
+countryBtn.onclick = function() {
+    modalCountry.style.display = "block";
+}
+//Close Modal
+closeBtn.onclick = function() {
+    modalCountry.style.display = "none";
+}
+//Close Modal When User Clicks Outside
+window.onclick = function(event) {
+    if (event.target == modalCountry) {
+      modalCountry.style.display = "none";
+    }
+  }
+
+//Weather Modal
+//Getting Weather Modal
+var modalWeather = document.getElementById("modalWeather");
+//Getting Weather Modal Btn
+var weatherBtn = document.getElementById("weatherBtn");
+//Get Close Weather Modal
+var closeBtn = document.getElementById("closeBtnWeather");
+//Open Modal
+weatherBtn.onclick = function() {
+    modalWeather.style.display = "block";
+}
+//Close Modal
+closeBtn.onclick = function() {
+    modalWeather.style.display = "none";
+}
+//Close Modal When User Clicks Outside
+window.onclick = function(event) {
+    if (event.target == modalWeather) {
+      modalWeather.style.display = "none";
+    }
+  }
