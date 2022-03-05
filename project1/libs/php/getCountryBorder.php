73716b409d753d6e1c5a5de6e132d0cd9e21fccb
Jasmine Taylor <83115753+j4sm1ne96@users.noreply.github.com>
2022-03-05 17:52:43 +0000

    Update getCountryBorder.php

--- project1/libs/php/getCountryBorder.php
+++ project1/libs/php/getCountryBorder.php
@@ -4,6 +4,16 @@ $json = file_get_contents('countryBorders.geo.json');
 
 $decoded_json = json_decode($json, true);
 
-L.geoJSON($decoded_json.geometry)
+$executionStartTime = microtime(true);
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data']['border'] = $border;
+	
+header('Content-Type: application/json; charset=UTF-8');
+
+echo json_encode($output); 
 
 ?>
