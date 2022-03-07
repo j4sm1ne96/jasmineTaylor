bf742f5c0a5fa4a080ea0ca2fa28bb36ffc831a1
Jasmine Taylor <83115753+j4sm1ne96@users.noreply.github.com>
2022-03-07 10:16:52 +0000

    Update getCountryBorder.php

--- project1/libs/php/getCountryBorder.php
+++ project1/libs/php/getCountryBorder.php
@@ -4,6 +4,15 @@ $executionStartTime = microtime(true);
 
 $countryBorders = json_decode(file_get_contents('countryBorders.geo.json'), true);
 
+$border = null;
+
+foreach ($countryBorders['feature'] as $feature {
+  if($feature["properties"]["ISO_A3"] == $_REQUEST['countryCode']) {
+    $border = $feature;
+    break;
+  }
+}
+
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
