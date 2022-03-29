a9cbb65c5593e18a1807518768a657306766faf6
Jasmine Taylor <83115753+j4sm1ne96@users.noreply.github.com>
2022-03-29 19:12:46 +0100

    Update getCountryBorder.php

--- project1/libs/php/getCountryBorder.php
+++ project1/libs/php/getCountryBorder.php
@@ -1,24 +1,22 @@
 <?php
 
-$executionStartTime = microtime(true);
+$executionStartTime = microtime(true) / 1000;
 
-$countryBorders = json_decode(file_get_contents('countryBorders.geo.json'), true);
+$result = file_get_contents('../json/countryBorders.geo.json');
 
-$border = null;
+$border = json_decode($result,true);
+$countryInfo = json_decode($result,true);
 
-foreach ($countryBorders['features'] as $feature) {
-  if($feature["properties"]["iso_a2"] == $_REQUEST['countryCode']) {
-    $border = $feature;
-    break;
-  }
-}
 
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
-$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
 $output['data']['border'] = $border;
-	
+$output['data']['countryInfo'] = $countryInfo;
+    
+//repeat above line for each API result
+
 header('Content-Type: application/json; charset=UTF-8');
 
 echo json_encode($output); 
